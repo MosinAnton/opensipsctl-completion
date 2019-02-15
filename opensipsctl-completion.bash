732174108f4849d9394fac59b085300b641ca48a
Anton Mosin <anton@mosin.org>
2019-02-15 17:20:23 +0500

    Create opensipsctl-completion.bash

---
+++ opensipsctl-completion.bash
@@ -0,0 +1,25 @@
+#/usr/bin/env bash
+
+_opensipsctl_completions()
+{
+  COMPREPLY=()
+  cur=${COMP_WORDS[COMP_CWORD]}
+  prev=${COMP_WORDS[COMP_CWORD-1]}
+
+  if [ $COMP_CWORD -eq 1 ]; then
+    COMPREPLY=( $(compgen -W "dispatcher fifo monitor ps" -- $cur) )
+  elif [ $COMP_CWORD -eq 2 ]; then
+    case "$prev" in
+      "dispatcher")
+        COMPREPLY=($(compgen -W "show reload dump addgw rmgw" -- $cur) )
+        ;;
+      "fifo")
+	      COMPREPLY=($(compgen -W "$(opensipsctl fifo which)" -- $cur) )
+        ;;
+      *)
+        ;;
+    esac
+  fi
+}
+
+complete -F _opensipsctl_completions opensipsctl

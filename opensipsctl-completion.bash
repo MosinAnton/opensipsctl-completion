9ce731a6f93735e65d64c4d0972fe97d71b709f2
Anton Mosin <anton@mosin.org>
2019-02-16 23:44:46 +0500

    Update opensipsctl-completion.bash

--- opensipsctl-completion.bash
+++ opensipsctl-completion.bash
@@ -1,5 +1,8 @@
 #/usr/bin/env bash
 
+# Copyright (C) 2019 Anton Mosin <anton@mosin.org>
+# Distributed under the GNU General Public License, version 2.0.
+
 _opensipsctl_completions()
 {
   COMPREPLY=()
